a074e4552dc1115919c56c518d0e45dd2450b1d4
iamsuraj-01 <ssemwal181096@gmail.com>
2024-08-29 11:51:32 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -93,7 +93,27 @@
                 items:3
             }
         }
-    });
+    })
+
+    $(document).ready(function(){
+        $(".reservation-carousel").owlCarousel({
+          loop: true,
+          margin: 10,
+          nav: true,
+          responsive:{
+              0:{
+                  items:1
+              },
+              600:{
+                  items:1
+              },
+              1000:{
+                  items:1
+              }
+          }
+        });
+      });
+      
 
     // Gallery Slider
     $('#autoWidth').lightSlider({
